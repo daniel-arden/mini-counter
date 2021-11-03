5b5da703912992abed9901772e78fefc0da9cb1d
Daniel Arden <daniel.arden1@gmail.com>
2021-11-03 15:24:16 +0100

    Initial Commit

---
+++ Mini Counter WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Mini Counter WatchKit Extension
+//
+//  Created by Daniel Arden on 03.11.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Mini Counter WatchKit Extension/Mini_CounterApp.swift
@@ -0,0 +1,19 @@
+//
+//  Mini_CounterApp.swift
+//  Mini Counter WatchKit Extension
+//
+//  Created by Daniel Arden on 03.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct Mini_CounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
